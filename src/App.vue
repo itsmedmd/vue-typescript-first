e2d3f662bb52240ff6f950bfafd49fb7e3b68fed
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-01-10 22:11:25 +0200

    Component data and methods with Options API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app">
-    <h1>helloo</h1>
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('zel')">change name</button>
+    <button @click="changeAge(47)">change age</button>
   </div>
 </template>
 
@@ -8,7 +10,22 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 50 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+    }
+  }
 });
 </script>
 
